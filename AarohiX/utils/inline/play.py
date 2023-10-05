d406faf64924dcf889f128d4584088b921ec1bec
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2023-10-05 16:05:50 +0530

    Update play.py

--- AarohiX/utils/inline/play.py
+++ AarohiX/utils/inline/play.py
@@ -33,6 +33,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             InlineKeyboardButton(
                 text="II", callback_data=f"ADMIN Pause|{chat_id}"
             ),
+                text="💖", callback_data=f"add_playlist {videoid}"
+            ),
             InlineKeyboardButton(
                 text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
             ),
@@ -95,6 +97,8 @@ def telegram_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(
                 text="II", callback_data=f"ADMIN Pause|{chat_id}"
             ),
+                text="💖", callback_data=f"add_playlist {videoid}"
+            ),
             InlineKeyboardButton(
                 text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
             ),
@@ -131,6 +135,8 @@ def stream_markup(_, videoid, chat_id):
             ),
             InlineKeyboardButton(
                 text="II", callback_data=f"ADMIN Pause|{chat_id}"
+            ),
+                text="💖", callback_data=f"add_playlist {videoid}"
             ),
             InlineKeyboardButton(
                 text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
@@ -174,6 +180,8 @@ def telegram_markup(_, chat_id):
             ),
             InlineKeyboardButton(
                 text="II", callback_data=f"ADMIN Pause|{chat_id}"
+            ),
+                text="💖", callback_data=f"add_playlist {videoid}"
             ),
             InlineKeyboardButton(
                 text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
@@ -322,6 +330,8 @@ def queue_markup(_, videoid, chat_id):
             ),
             InlineKeyboardButton(
                 text="II", callback_data=f"ADMIN Pause|{chat_id}"
+            ),
+                text="💖", callback_data=f"add_playlist {videoid}"
             ),
             InlineKeyboardButton(
                 text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
