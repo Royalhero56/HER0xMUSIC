3a5113287ba94e7d35daba71f19c66a24f4bb638
Royalhero56 <158155141+Royalhero56@users.noreply.github.com>
2024-01-31 08:45:26 +0530

    Update Shayari.py

--- AarohiX/plugins/Shayari.py
+++ AarohiX/plugins/Shayari.py
@@ -66,9 +66,9 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🥰𝐎𝐖𝐍𝐄𝐑🥰", url=f"https://t.me/iamcutehero"),
+                        "💕𝐎𝐖𝐍𝐄𝐑💕", url=f"https://t.me/iamcutehero"),
                     InlineKeyboardButton(
-                        "🧸𝐁𝐑𝐎𝐓𝐇𝐄𝐑🧸", url=f"https://t.me/Royal_king365")
+                        "💓𝐁𝐑𝐎𝐓𝐇𝐄𝐑💓", url=f"https://t.me/@Ayuuubby")
                     
                 ]
             ]
@@ -86,9 +86,9 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🥰𝐎𝐖𝐍𝐄𝐑🥰", url=f"https://t.me/iamcutehero"),
+                        "💕𝐎𝐖𝐍𝐄𝐑💕", url=f"https://t.me/iamcutehero"),
                     InlineKeyboardButton(
-                        "🧸𝐁𝐑𝐎𝐓𝐇𝐄𝐑🧸", url=f"https://t.me/Royal_king365")
+                        "💓𝐁𝐑𝐎𝐓𝐇𝐄𝐑💓", url=f"https://t.me/@Ayuuubby")
                     
                 ]
             ]
